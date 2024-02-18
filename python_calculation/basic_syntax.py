31be7454fc166d4e69cf10ac18856c9bfdce8377
mitsuki <ai.takamitsu@gmail.com>
2024-02-18 10:58:47 +0900

    python basic_syntax

---
+++ python_calculation/basic_syntax.py
@@ -0,0 +1,11 @@
+
+def variable():
+    # 変数定義
+    x = 5
+    print(x)
+    # 変数の再定義
+    x = x + 1
+    print(x)
+    # 変数に再代入
+    x = 1
+    print(x)
---
+++ python_calculation/main.py
@@ -0,0 +1,9 @@
+import basic_syntax
+
+
+def main():
+    basic_syntax.variable()
+
+
+if __name__ == '__main__':
+    main()
